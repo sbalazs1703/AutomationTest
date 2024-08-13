1403a153ae413d6a4eb62535acd9424edae98aa5
Simon Balazs <s.balazs.laszlo@gmail.com>
2024-08-13 23:58:53 +0200

    LoginTest

--- src/test/java/AutomationTest1.java
+++ src/test/java/AutomationTest1.java
@@ -1,19 +1,53 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class AutomationTest1 {
 
+    public static ChromeOptions options;
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public static void Setup() {
+        options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-search-engine-choice-screen");
+
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/Resources/chromedriver.exe");
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+    }
 
 
     @Test
-    public void Setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/Resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver(options);
-
+    void Teststeps() throws InterruptedException {
         driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+
+
+        driver.findElement(By.xpath("//input[@type=\"checkbox\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href=\"SignIn.html\"]")).click();
+
+        driver.findElement(By.xpath("//input[@id=\"usr\"]")).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id=\"usr\"]")).sendKeys("testuser");
+        driver.findElement(By.xpath("//input[@id=\"pwd\"]")).sendKeys("password");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@value=\"Login\"]")).click();
+
+        driver.findElement(By.xpath("//button[@class=\"btn btn-info FormalShoes\"] "));
+        Thread.sleep(2000);
+    }
+    @AfterTest
+    void CloseBrowser(){
+        driver.close();
     }
 }
+
